b8a7a35ce424b41bd89a133158f489693dd9e0e2
casper <133casper@gmail.com>
2020-05-12 10:52:47 +0300

    Add sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -8,17 +8,13 @@ use Twig\Loader\FilesystemLoader;
 
 require_once 'vendor/autoload.php';
 
-// TODO: links.php page
-// TODO: edit_link.php page
-// TODO: delete_link.php page
-
 $loader = new FilesystemLoader('templates');
 $twig = new Environment($loader, [
     'cache' => 'vendor/cache',
 ]);
 
 try {
-    echo $twig->render('main_page.twig');
+    echo $twig->render('sign_in_page.twig');
 } catch (LoaderError | RuntimeError | SyntaxError $e) {
     echo $e->getMessage();
 }
